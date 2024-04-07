c248bf26d6c48cba13352a286203dd1d3b2d9ad9
Rechnoe <temirlantashen4@gmail.com>
2024-04-08 01:31:58 +0600

    Updated 08.04

--- frontend/air_web_app/src/app/app.component.ts
+++ frontend/air_web_app/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HotelComponent } from './hotel/hotel.component';
+import { SelectorComponent } from './selector/selector.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, CarouselComponent, MenuBarComponent, FooterComponent],
+  imports: [RouterOutlet, NavBarComponent, CarouselComponent, MenuBarComponent, FooterComponent, HotelComponent, SelectorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/air_web_app/src/app/nav-bar/nav-bar.component.ts
+++ frontend/air_web_app/src/app/nav-bar/nav-bar.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
