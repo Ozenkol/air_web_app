73eeeb1dc48a4c8df3529b697bbde0312f6b13a6
Rechnoe <temirlantashen4@gmail.com>
2024-04-09 00:27:56 +0600

    09.04 commit v2

--- frontend/air_web_app/src/app/home/home.component.ts
+++ frontend/air_web_app/src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { CarouselComponent } from '../carousel/carousel.component';
 import { MenuBarComponent } from '../menu-bar/menu-bar.component';
 import { DirectionComponent } from '../direction/direction.component';
 import { SelectorComponent } from '../selector/selector.component';
+import { NewsComponent } from '../news/news.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent, MenuBarComponent, DirectionComponent, SelectorComponent],
+  imports: [CarouselComponent, MenuBarComponent, DirectionComponent, SelectorComponent, NewsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
