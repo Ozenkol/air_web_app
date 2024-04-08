21f1d0d2339097c26604b4691e2a1c4d9ce0e1b3
Rechnoe <temirlantashen4@gmail.com>
2024-04-09 03:57:27 +0600

    09.04 commit v3

--- frontend/air_web_app/src/app/carousel/carousel.component.ts
+++ frontend/air_web_app/src/app/carousel/carousel.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuBarComponent } from '../menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [],
+  imports: [MenuBarComponent],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css'
 })
