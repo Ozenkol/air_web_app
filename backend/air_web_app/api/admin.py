1d7d1394aaaf3eff7e5e9287f5aae51ec13f182c
Arslan <alan.aquinasss@gmail.com>
2024-04-09 10:09:15 +0600

    register models

--- backend/air_web_app/api/admin.py
+++ backend/air_web_app/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Flight, Passenger, Booking, Airport
+
+admin.site.register(Flight)
+admin.site.register(Passenger)
+admin.site.register(Booking)
+admin.site.register(Airport)
